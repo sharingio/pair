1f2720d088a4cb8b236a4979b502ece98bdfa8aa
zach <zz@ii.coop>
2020-10-20 14:40:09 +1300

    grab email for authenticated user, even if private

--- src/syme/web.clj
+++ src/syme/web.clj
@@ -48,6 +48,13 @@
                 {:headers {"accept" "application/json"}})
       (:body)(json/decode true)))
 
+(defn get-email
+  [username token]
+  (-> (http/get (str "https://api.github.com/user/email?access_token="token)
+                {:headers {"accept" "application/json"}})
+      (:body)(json/decode true)
+      (first)))
+
 (def app
   (routes
    (GET "/" {{:keys [username]} :session}
@@ -123,7 +130,7 @@
         (if code
           (let [token (get-token code)
                 {username :login
-                 email :email
+                 email (get-email username token)
                  fullname :name} (get-user token)
                  orgs (get-orgs username token)
                  sharingio-member ((complement empty?)(filter #(= "sharingio" (:login %)) orgs))]
