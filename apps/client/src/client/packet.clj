59873f3ade394fd13006d7c2251494a952c24d3f
Zach Mandeville <zz@ii.coop>
2020-11-20 04:13:01 +0000

    get all instances and pretty print yaml

--- apps/client/src/client/packet.clj
+++ apps/client/src/client/packet.clj
@@ -1,9 +1,11 @@
 (ns client.packet
+  (:refer-clojure :exclude [load])
   (:require [cheshire.core :as json]
             [next.jdbc :as jdbc]
             [next.jdbc.result-set :as rs]
             [clojure.tools.logging :as log]
             [clj-http.client :as http]
+            [yaml.core :as yaml]
             [environ.core :refer [env]]
             [client.db :as db])
   (:use [slingshot.slingshot :only [throw+ try+]]))
@@ -28,24 +30,24 @@
                      (:body)
                      (json/decode true))
         {{api-response :response} :metadata
-               {phase :phase} :status
-               {name :name} :spec} response]
+         {phase :phase} :status
+         {name :name} :spec} response]
     {:owner username
-    :project project
-    :facility facility
-    :type type
-    :guests guests
-    :instance-id name
-    :status (str api-response": "phase)}))
+     :project project
+     :facility facility
+     :type type
+     :guests guests
+     :instance-id name
+     :status (str api-response": "phase)}))
 
 (defn instance-phase
   [instance-id]
   (let [phase (try+ (-> (http/get (str backend-address"/api/instance/kubernetes/"instance-id))
-                         :body
-                         (json/decode true) :status :phase)
-                     (catch Object _ ;; The first time it is pinged we sometimes get an HTTPNoResponse
-                       (log/error "no http response for instance " instance-id)
-                       "Not Ready"))]
+                        :body
+                        (json/decode true) :status :phase)
+                    (catch Object _ ;; The first time it is pinged we sometimes get an HTTPNoResponse
+                      (log/error "no http response for instance " instance-id)
+                      "Not Ready"))]
     {:instance-id instance-id
      :phase phase}))
 
@@ -59,29 +61,80 @@
           "Not Ready")))
 
 (defn get-kubeconfig
-  [{:keys [phase instance_id]}]
+  [phase instance-id]
   (if (= "Provisioning" phase) nil
-    (try+ (-> (http/get (str backend-address"/api/instance/kubernetes/"instance_id"/kubeconfig"))
-              :body (json/decode true)
-              :spec json/generate-string)
-          (catch Object _
-            (log/error "tried to get kubeconfig, no luck for " instance_id)
-            nil))))
+      (try+ (-> (http/get (str backend-address"/api/instance/kubernetes/"instance-id"/kubeconfig"))
+                :body (json/decode true)
+                :spec json/generate-string
+                yaml/parse-string
+                (yaml/generate-string :dumper-options {:flow-style :block}))
+            (catch Object _
+              (log/error "tried to get kubeconfig, no luck for " instance-id)
+              nil))))
 
-(defn get-tmate
-  [{:keys [kubeconfig instance_id]}]
+(defn get-tmate-ssh
+  [kubeconfig instance_id]
   (if (nil? kubeconfig) "Not ready to fetch tmate session"
-      (try+ (-> (http/get (str backend-address"/api/instance/kubernetes/"instance_id"/tmate"))
+      (try+ (-> (http/get (str backend-address"/api/instance/kubernetes/"instance_id"/tmate/ssh"))
                 :body (json/decode true) :spec)
             (catch Object _
               (log/error "tried to get tmate, no luck for " instance_id)
               "No Tmate session yet"))))
 
-(defn zaunch
-  [username params]
-  (println "ZAUNCH" username params)
-  {:instance-id "zach-1sc00l"})
+(defn get-tmate-web
+  [kubeconfig instance-id]
+  (if (nil? kubeconfig) "Not ready to fetch tmate session"
+      (try+ (-> (http/get (str backend-address"/api/instance/kubernetes/"instance-id"/tmate/web"))
+                :body (json/decode true) :spec)
+            (catch Object _
+              (log/error "tried to get tmate, no luck for " instance-id)
+              "No Tmate session yet"))))
 
+(defn zaunch
+  [username {:keys [project facility type guests fullname email repos] :as params}]
+  (println params)
+  (let [backend (str "http://"(env :backend-address)"/api/instance")
+        instance-spec {:type type
+                       :facility facility
+                       :setup {:user username
+                               :guests (if (empty? guests)
+                                         [ ]
+                                         (clojure.string/split guests #" "))
+                               :repos (if (empty? repos)
+                                        [ ]
+                                        (clojure.string/split repos #" "))
+                               :fullname fullname
+                               :email email}}
+        response (-> (http/post backend {:form-params instance-spec :content-type :json})
+                     (:body)
+                     (json/decode true))
+        {{api-response :response} :metadata
+         {phase :phase} :status
+         {name :name} :spec} response]
+    (println "INSTANCE SPEC" instance-spec)
+    {:owner username
+     :facility facility
+     :type type
+     :tmate-ssh nil
+     :tmate-web nil
+     :kubeconfig nil
+     :guests guests
+     :instance-id name
+     :name name
+     :status (str api-response": "phase)}))
+
+(defn get-all-instances
+  [username]
+  (let [instances (try+ (-> (http/get (str backend-address"/api/instance/kubernetes"))
+                            :body (json/decode true))
+                        (catch Object _
+                          (log/warn "Couldn't get instances")
+                          []))]
+    (println )
+                        )])
+  (-> (http/get (str backend-address"/api/instance/kubernetes/zachmandeville-1i0q/tmate/ssh"))
+  ;;     :body (json/decode true) :spec)
+  [{:owner username :instance-id "zach-is-still-cool"}])
 
 ;; #+RESULTS: get all names of Kubernetes instances
 ;; #+begin_example
@@ -109,9 +162,21 @@
 ;; curl -X GET http://localhost:8080/api/instance/kubernetes | jq .
 ;; #+end_src
 
-;; (-> (http/get (str backend-address"/api/instance/kubernetes/zachmandeville-1i0q/tmate/web"))
+;; (def instance-id "zachmandeville-4zjn")
 
+
+
+;; (-> (http/get (str backend-address"/api/instance/kubernetes/zachmandeville-4zjn/tmate/web"))
 ;;           :body (json/decode true) :spec)
 
 ;; (-> (http/get (str backend-address"/api/instance/kubernetes/zachmandeville-1i0q/tmate/ssh"))
 ;;     :body (json/decode true) :spec)
+
+;; (-> (http/get (str backend-address"/api/instance/kubernetes"))
+;; :body (json/decode true)
+;; :list))
+
+;; (map :spec))
+
+
+;; {:name })
