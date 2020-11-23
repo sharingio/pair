cbaaba57df915042fb1af32ad3eb50d936713c7f
Zach Mandeville <zz@ii.coop>
2020-11-23 22:58:32 +0000

    add spec for gh types

--- apps/client/src/client/github.clj
+++ apps/client/src/client/github.clj
@@ -4,20 +4,14 @@
             [clojure.tools.logging :as log]
             [clojure.spec.alpha :as s]
             [clojure.spec.test.alpha :as test]
-            [clj-http.client :as http])
+            [clj-http.client :as http]
+            [client.spec :as spec])
 (:use [slingshot.slingshot :only [throw+ try+]]))
 
-
-(def github-username-regex #"^([A-Za-z\d]+-)*[A-Za-z\d]+$")
-
-(s/def ::username (s/and string? #(re-matches github-username-regex %)))
-
-(s/def ::gh-oauth-code string?)
 
 (s/fdef get-token
   :args (s/cat :code ::gh-oauth-code)
   :ret (s/nilable string?))
-
 
 (defn get-token [code]
   (-> (http/post "https://github.com/login/oauth/access_token"
@@ -27,28 +21,22 @@
                   :headers {"Accept" "application/json"}})
       :body (json/decode true) :access_token))
 
-(defn github-get
+(defn get
   [endpoint token]
   (-> (http/get (str "https://api.github.com/" endpoint)
                 {:headers {"accept" "application/json"
                            "Authorization" (str "token " token)}})
       :body (json/decode true)))
 
-(defn get-primary-email
+(defn get-raw-info
   [token]
-  (let [emails (github-get "user/emails" token)]
-    (:email (first (filter #(= (:primary %)true) emails)))))
+  {:user (get "user" token)
+   :emails (get "user/emails" token)
+   :orgs (get "user/orgs" token)})
+
 
 (defn in-permitted-org?
   [token]
   (let [user-orgs (set (map :login (github-get "user/orgs" token)))
         permitted-orgs (set '(sharingio cncf kubernetes))]
     (empty? (clojure.set/intersection user-orgs permitted-orgs))))
-
-(defn get-repo
-  [project]
-  (try+ (-> (http/get (str "https://api.github.com/repos/" project)
-                {:headers {"accept" "application/json"}})
-            :body (json/decode true))
-        (catch [:status 404] {:keys [request-time headers body]}
-          (log/warn "404" request-time project headers body))))
--- apps/client/src/client/spec.clj
+++ apps/client/src/client/spec.clj
@@ -8,15 +8,23 @@
 
 (s/def ::email (s/and string? #(re-matches email-regex %)))
 (s/def ::username (s/and string? #(re-matches github-username-regex %)))
+
+(s/def :gh/token string?)
+(s/def :gh/primary (s/nilable boolean?))
+(s/def :gh/verified (s/nilable boolean?))
+(s/def :gh/email (s/keys :req-un [::email :gh/primary :gh/verified ::visibility]))
+(s/def :gh/emails (s/coll-of :gh/email))
+(s/def :gh/user (s/keys :req-un [::html_url ::email ::name ::login ::url ::location]))
+(s/def :gh/org (s/keys :req-un [::description ::login
+                                ::node_id ::id
+                                ::url ::events_url
+                                ::issues_url ::hooks_url
+                                ::members_url ::public_members_url
+                                ::repos_url ::avatar_url]))
+(s/def :gh/orgs (s/coll-of :gh/org))
+(s/def :gh/raw-info (s/keys :req-un [:gh/orgs :gh/user :gh/emails]))
+
 (s/def ::permitted-member boolean?)
 (s/def ::avatar string?)
 (s/def ::profile-url string?)
-(s/def ::gh-token string?)
-
-(s/def ::user (s/keys :req-un [::username ::email ::permitted-member ::avatar ::profile-url ::gh-token]))
-(s/valid? ::user {:username "zachmandeville"
-                  :email "zz@ii.coop"
-                  :permitted-member true
-                  :avatar "https://github.com/avatar"
-                  :profile-url "https://github.com/zachmandeville"
-                  :gh-token "zjdkj198203ekj39802"})
+(s/def ::user (s/keys :req-un [::username ::email ::permitted-member ::avatar ::profile-url :gh/token]))
