36cd9f76ab444b2846f4551562e783cab12e96c8
Zach Mandeville <zz@ii.coop>
2020-11-23 21:01:21 +0000

    add spec for user

--- apps/client/src/client/spec.clj
+++ apps/client/src/client/spec.clj
@@ -4,11 +4,19 @@
    [clojure.spec.test.alpha :as test]))
 
 (def github-username-regex #"^([A-Za-z\d]+-)*[A-Za-z\d]+$")
+(def email-regex #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$")
 
+(s/def ::email (s/and string? #(re-matches email-regex %)))
 (s/def ::username (s/and string? #(re-matches github-username-regex %)))
-(s/valid? ::username "zachmandeville")
-(s/valid? ::username "BobyMcBobs")
-(s/valid? ::username "cool-h2and-57l-luke")
-(s/valid? ::username "-should-be-false!")
+(s/def ::permitted-member boolean?)
+(s/def ::avatar string?)
+(s/def ::profile-url string?)
+(s/def ::gh-token string?)
 
-(s/def ::gh-oauth-code string?)
+(s/def ::user (s/keys :req-un [::username ::email ::permitted-member ::avatar ::profile-url ::gh-token]))
+(s/valid? ::user {:username "zachmandeville"
+                  :email "zz@ii.coop"
+                  :permitted-member true
+                  :avatar "https://github.com/avatar"
+                  :profile-url "https://github.com/zachmandeville"
+                  :gh-token "zjdkj198203ekj39802"})
