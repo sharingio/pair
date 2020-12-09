555d79ea85ceb547b53f06636e8219881af99948
Zach! <webmaster@coolguy.website>
2020-12-10 09:27:56 +1300

    better top nav, better styling

--- docs/_firn/partials/nav.clj
+++ docs/_firn/partials/nav.clj
@@ -1,4 +1,13 @@
 (defn nav
   [title subtitle]
-  [:nav
-   [:a.title {:href "/"} title [:sup subtitle]]])
+  [:nav#top
+   [:a.title {:href "/"} title [:sup subtitle]]
+   [:div.ext-links
+    [:a {:href "https://sharing.io"
+         :target "_blank"
+         :rel "noreferrer noopener"}
+     "home"]
+    [:a {:href "https://github.com/sharingio/pair"
+         :target "_blank"
+         :rel "noreferrer noopener"}
+     "source"]]])
