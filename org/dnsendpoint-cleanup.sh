2479e8c88228413c98ee34e16f4f814a1deb4a2c
Sharing IO <sharingio@ii.coop>
2021-04-20 14:58:57 +1200

    Update org/cleanup

--- org/dnsendpoint-cleanup.sh
+++ org/dnsendpoint-cleanup.sh
@@ -5,13 +5,18 @@
 
 
 LIVE_INSTANCES=$(curl http://sharingio-pair-clusterapimanager.sharingio-pair:8080/api/instance 2>/dev/null | jq -r '.list[].spec.name')
-DNSENDPOINTS=$(kubectl -n sharingio-pair get dnsendpoints | awk '{print $1}' | grep -E '[a-z0-9-]' | xargs -0)
+DNSENDPOINTS=$(kubectl -n sharingio-pair get dnsendpoints -o=jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
 
+DNSENDPOINTS_TO_DELETE=""
 for DNSENDPOINT in $DNSENDPOINTS; do
-  kubectl -n sharingio-pair get dnsendpoint $DNSENDPOINT -o=jsonpath='{.metadata.labels}' | jq -r '."io.sharing.pair-spec-name"'
-  if echo $LIVE_INSTANCES | grep -q $(kubectl -n sharingio-pair get dnsendpoint $DNSENDPOINT -o=jsonpath='{.metadata.labels}' | jq -r '."io.sharing.pair-spec-name"') ; then
-    echo "yes: $DNSENDPOINT"
+  INSTANCE_NAME=$(kubectl -n sharingio-pair get dnsendpoint $DNSENDPOINT -o=jsonpath="{.metadata.labels."io\\.sharing\\.pair-spec-name"}")
+  if echo $LIVE_INSTANCES | grep -q -E "(^| )$INSTANCE_NAME( |$)"; then
+    echo "Instance '$INSTANCE_NAME' is alive"
   else
-    echo "no : $DNSENDPOINT"
+    echo "Instance '$INSTANCE_NAME' not found, removing DNSEndpoint"
+    DNSENDPOINTS_TO_DELETE="$DNSENDPOINTS_TO_DELETE $DNSENDPOINT"
   fi
 done
+if [ ! -z "$DNSENDPOINTS_TO_DELETE" ] ; then
+  kubectl -n sharingio-pair delete dnsendpoint $DNSENDPOINTS_TO_DELETE
+fi
