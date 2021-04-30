26c23c2fea92940c7739ff7c83e5e87702ead93b
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-04-30 14:59:28 +1200

    Update .sharing.io/prepare-env.sh

--- .sharing.io/prepare-env.sh
+++ .sharing.io/prepare-env.sh
@@ -12,20 +12,21 @@ echo "      go to an existing or new OAuth app."
 echo "      ensure that:"
 echo "        - homepage URL is set to 'https://pair.${SHARINGIO_PAIR_BASE_DNS_NAME}'"
 echo "        - authorization callback URL is set to 'https://pair.${SHARINGIO_PAIR_BASE_DNS_NAME}/oauth'"
+echo
+echo "Input:"
 read -r -p "OAUTH_CLIENT_ID (github oauth app client id)                  : " OAUTH_CLIENT_ID
 read -r -p "OAUTH_CLIENT_SECRET (github oauth app client generated secret): " OAUTH_CLIENT_SECRET
-
+echo
 echo "Appending to '$GIT_ROOT/.env'"
 cat <<EOF >> $GIT_ROOT/.env
 OAUTH_CLIENT_ID=${OAUTH_CLIENT_ID}
 OAUTH_CLIENT_SECRET=${OAUTH_CLIENT_SECRET}
 EOF
-
 touch $GIT_ROOT/.sharing.io/setup-complete
-
 echo
 echo "Please that note that it may take a minute to bring everything up."
 echo
 echo "Access Pair's client/frontend in development from 'https://pair.${SHARINGIO_PAIR_BASE_DNS_NAME}'"
 echo "Access Pair's backend from 'http://localhost:8080/api'"
+echo
 read -r -p "Press enter to exit"
