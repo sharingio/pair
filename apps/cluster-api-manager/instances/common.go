352e81d85659a7deb537afbb545f612e4f54a7df
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-06-04 16:00:57 +1200

    Update default instance humacs version

--- apps/cluster-api-manager/instances/common.go
+++ apps/cluster-api-manager/instances/common.go
@@ -15,7 +15,7 @@ var (
 	instanceDefaultNodeSize          = "c1.small.x86"
 	instanceDefaultTimezone          = "Pacific/Auckland"
 	instanceDefaultHumacsRepository  = "registry.gitlab.com/humacs/humacs/ii"
-	instanceDefaultHumacsVersion     = "2021.05.27.2050"
+	instanceDefaultHumacsVersion     = "2021.06.02.0218"
 	instanceDefaultKubernetesVersion = "1.21.0"
 )
 
